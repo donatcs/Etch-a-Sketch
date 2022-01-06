8a6afdd3be5b2f9d710e240bfd1e6b56aa36dba6
donatcs <96204719+donatcs@users.noreply.github.com>
2022-01-06 21:39:19 +0100

    updated

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+
 let container = document.getElementById('container');
 
 let ROWS = 16
 let COLS = 16
 let PIXEL = 30
+
 
 
 function containerAdd () {
@@ -11,14 +13,22 @@ function containerAdd () {
             for (let j = 0; j < COLS; j++) {
                         let pixel = document.createElement('div');
                         pixel.style.position = 'absolute';
-                        pixel.style.border = '1px solid red'
+                        pixel.style.border = '1px solid green'
                         pixel.style.left = j * PIXEL + 'px';
                         pixel.style.top = i * PIXEL + 'px';
                         pixel.style.width = PIXEL  + 'px';
                         pixel.style.height = PIXEL + 'px';
+                        
                         container.appendChild(pixel)
+                
+                        pixel.style.backgroundColor = 'black'
+
             }
         }
 }
 
+
+
 containerAdd ();
+
+
