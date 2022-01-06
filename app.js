de76def73dfa291b593506a22f86df4e402a539d
donatcs <csonkadonaaat@gmail.com>
2022-01-06 18:35:28 +0100

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-<script>
+let container = document.getElementById('container');
+
+let ROWS = 16
+let COLS = 16
+let PIXEL = 30
 
 
-</script>
+function containerAdd () {
+
+        for (let i = 0; i < ROWS; i++) {
+            for (let j = 0; j < COLS; j++) {
+                        let pixel = document.createElement('div');
+                        pixel.style.position = 'absolute';
+                        pixel.style.border = '1px solid red'
+                        pixel.style.left = j * PIXEL + 'px';
+                        pixel.style.top = i * PIXEL + 'px';
+                        pixel.style.width = PIXEL  + 'px';
+                        pixel.style.height = PIXEL + 'px';
+                        container.appendChild(pixel)
+            }
+        }
+}
+
+containerAdd ();
