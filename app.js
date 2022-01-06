a51ef116c298875991d953899fe808d2e085003d
donatcs <96204719+donatcs@users.noreply.github.com>
2022-01-07 00:02:51 +0100

    updated 1/7

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function containerAdd () {
         for (let i = 0; i < ROWS; i++) {
             for (let j = 0; j < COLS; j++) {
                         let pixel = document.createElement('div');
+                        pixel.classList.add("weird");
                         pixel.style.position = 'absolute';
                         pixel.style.border = '1px solid green'
                         pixel.style.left = j * PIXEL + 'px';
@@ -21,14 +22,22 @@ function containerAdd () {
                         
                         container.appendChild(pixel)
                 
-                        pixel.style.backgroundColor = 'black'
+                        
 
             }
         }
 }
 
 
-
 containerAdd ();
 
+const weird = document.querySelectorAll(".weird");
 
+let changeColor = weird.forEach(weird => {  
+    weird.addEventListener("mouseover", () => {
+        const Color1 = Math.floor(Math.random() * 256);
+    const Color2 = Math.floor(Math.random() * 256);
+    const Color3 = Math.floor(Math.random() * 256);
+         weird.style.backgroundColor = `rgb(${Color1}, ${Color2}, ${Color3})`;
+    })
+})
